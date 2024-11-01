b3d304bfe053c9df9ba5004df989841a5d48672a
Milind Sangale <smilind900@gmail.com>
2024-11-01 09:31:36 +0530

    final commit

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -8,6 +8,10 @@ const quizData = [
         correct: "d",
     },
     {
+
+
+
+        
         question: "What does CSS stand for?",
         a: "Central Style Sheets",
         b: "Cascading Style Sheets",
